0959ce2e98694904dbc4a81000a436ca11285746
dheidebrecht <dan.heidebrecht@gmail.com>
2016-08-16 13:42:09 -0700

    add filters/codecs/muxers required for multiclip

--- ffmpeg_build.sh
+++ ffmpeg_build.sh
@@ -20,32 +20,29 @@ make clean
 --cross-prefix="$CROSS_PREFIX" \
 --arch="$NDK_ABI" \
 --cpu="$CPU" \
---enable-runtime-cpudetect \
 --sysroot="$NDK_SYSROOT" \
+--disable-everything \
+--enable-runtime-cpudetect \
 --enable-pic \
---enable-libx264 \
---enable-libass \
---enable-libfreetype \
---enable-libfribidi \
---enable-libmp3lame \
---enable-fontconfig \
 --enable-pthreads \
---disable-debug \
---disable-ffserver \
 --enable-version3 \
---enable-hardcoded-tables \
---disable-ffplay \
---disable-ffprobe \
---enable-gpl \
 --enable-yasm \
---disable-doc \
---disable-shared \
+--enable-decoder=aac,h264,mpeg2video,mpeg4,mp3,mjpeg,jpeg2000,libopenjpeg \
+--enable-encoder=aac,libx264,mp3,mpeg4,image2,mjpeg,jpeg2000,libopenjpeg \
+--enable-demuxer=aac,avi,h264,image2,matroska,pcm_s16le,mjpeg,mov,mp3,m4v,rawvideo,wav \
+--enable-filters \
+--enable-filter=anullsrc \
+--enable-indev=lavfi \
+--enable-muxer=h264,mp4,mjpeg,mp3 \
+--enable-parser=aac,h264,mjpeg,mpeg4video,mpegaudio,mpegvideo \
+--enable-gpl \
 --enable-static \
+--enable-libx264 \
+--enable-protocol=concat,file \
 --pkg-config="${2}/ffmpeg-pkg-config" \
 --prefix="${2}/build/${1}" \
 --extra-cflags="-I${TOOLCHAIN_PREFIX}/include $CFLAGS" \
 --extra-ldflags="-L${TOOLCHAIN_PREFIX}/lib $LDFLAGS" \
---extra-libs="-lpng -lexpat -lm" \
 --extra-cxxflags="$CXX_FLAGS" || exit 1
 
 make -j${NUMBER_OF_CORES} && make install || exit 1
